78fb4e94ed8c6a363b60ee62b84bd772421af378
Iggy <igoririanto@rocketmail.com>
2017-06-05 18:24:01 -0700

    start styling cards

--- src/schedules/DisplaySchedules.js
+++ src/schedules/DisplaySchedules.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import {Thumbnail, Row, Col, Button, Image} from 'react-bootstrap';
 import CalendarIcon from '../../public/assets/CalendarIcon.png';
+import Moment from 'moment';
 
 const thumbnailStyle= {
   backgroundColor: '#FF8383',
-  height: '500px'
+  padding: '100px 0px 100px 0px',
+  border: 'none'
 };
 
 const topRectangle = {
+}
 
+const textStyles = {
+  scheduleStyle: {
+    textAlign: 'center'
+  },
+  messageStyle: {
+    fontStyle: 'italic'
+  }
 }
 
 class DisplaySchedules extends React.Component{
   render(){
     return (
       <div>
+
       {this.props.schedules.map((schedule, index) =>
         <Col md={4} sm={6} key={index}>
           <Thumbnail src={CalendarIcon} style={thumbnailStyle}>
             <Row>
-              <Col xs={10} xsOffset={1}>
-                <h4 className="class-title">
-                  Schedule For: {schedule.date} (Schedule ID: {schedule.id})
-                </h4>
+              <Col xs={10} xsOffset={1} style={textStyles.scheduleStyle}>
+                <h2 className="class-title">
+                  {Moment(schedule.date).format('dddd')}
+                </h2>
+                <h5>
+                  {Moment(schedule.date).format('MMM DD YYYY')}
+                </h5>
+                <h6>(Schedule ID: {schedule.id})</h6>
               </Col>
             </Row>
             <Row>
               <Col xs={10} xsOffset={1}>
-                <h4>Workers:</h4>
                 {this.props.workers.filter(function(worker){
                   return worker.schedule_id === schedule.id
                 })
@@ -39,8 +53,7 @@ class DisplaySchedules extends React.Component{
             </Row>
             <Row>
               <Col xs={10} xsOffset={1}>
-                <h3>Message:</h3>
-                  {schedule.message}
+                <h5 style={textStyles.messageStyle}>{schedule.message}</h5>
               </Col>
             </Row>
             <Row>
@@ -57,6 +70,7 @@ class DisplaySchedules extends React.Component{
               </Col>
             </Row>
           </Thumbnail>
+
         </Col>
       )}
 
